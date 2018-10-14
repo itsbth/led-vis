1cb5787fc197c53db81b99b64aca7a52ebd079b0
Bjørn Tore Håvie <bjorn.haavie@inonit.no>
2018-10-14 15:19:51 +0200

    Basic functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import classnames from "classnames";
+import "./App.css";
+
+const VBARS = 20;
+
+const Bar = ({ n }) => (
+  <div className="bar">
+    {Array.from({ length: VBARS }).map((_, idx) => (
+      <div key={idx} className={classnames('tick', {
+        active: n > (idx * 50),
+        low: idx < 5,
+        mid: idx >= 5 && idx < 15,
+        high: idx >= 15
+      })} />
+    ))}
+  </div>
+);
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      bars: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    };
+  }
+  componentDidMount() {
+    this.socket = new WebSocket("ws://localhost:3001/");
+    this.socket.addEventListener("message", mess => {
+      this.setState({
+        bars: mess.data
+          .split(";")
+          .filter(n => n !== "")
+          .map(n => parseInt(n, 10))
+      });
+    });
+  }
+  componentWillUnmount() {
+    this.socket.close();
+  }
   render() {
+    const { bars } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {bars.map((n, idx) => (
+          <Bar key={idx} n={n} />
+        ))}
       </div>
     );
   }
